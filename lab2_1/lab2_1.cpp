2400a0cb81c1b781fb2f0373cf6a13ea825c0b34
spongdud18 <spongdud18@gmail.com>
2015-03-26 16:41:55 +0900

    Modified the cpp file
    Modified the cpp file to change lab2_1
    to my Lab Assignment #2-2 in Androx Studio
    of my local computer

--- lab2_1/lab2_1.cpp
+++ lab2_1/lab2_1.cpp
@@ -1,17 +1,37 @@
 // Do not remove the include below
 #include "lab2_1.h"
 
-#define FRONT_LED_PIN 10
-#define REAR_LED_PIN 9
+#define FRONT_LED_PIN   10
+#define REAR_LED_PIN   9
+#define DUTY_CYCLE   20   //20ms
 
-boolean increasing = true;
-int front_led_value = 0;
+int done = false;
+
+void PWM_Write(int pin, int on_time_perc)
+{
+	int on_time = DUTY_CYCLE*on_time_perc/100.0;
+	int off_time = DUTY_CYCLE - on_time;
+	
+	digitalWrite(pin, HIGH);
+	delay(on_time);
+	
+	digitalWrite(pin, LOW);
+	delay(off_time);
+}
+void myAnalogWrite(int pin, int percent, int time)
+{
+	int num_loops = time/DUTY_CYCLE;
+	int i;
+	for (i=0; i<num_loops; i++)
+	{
+		PWM_Write(pin, percent);
+	}
+}
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
 	// Add your initialization code here
-	pinMode(FRONT_LED_PIN, OUTPUT);
 	pinMode(REAR_LED_PIN, OUTPUT);
 }
 
@@ -19,21 +39,13 @@ void setup()
 void loop()
 {
 	//Add your repeated code here
-	analogWrite(FRONT_LED_PIN, front_led_value);
-	analogWrite(REAR_LED_PIN, 255-front_led_value);
-	delay(20);
-	
-	if (increasing == true)
-		front_led_value++;
+	if (done == false) {
+		myAnalogWrite(REAR_LED_PIN, 100, 2000);
+		myAnalogWrite(REAR_LED_PIN, 75, 2000);
+		myAnalogWrite(REAR_LED_PIN, 50, 2000);
+		myAnalogWrite(REAR_LED_PIN, 25, 2000);
+		done = true;
+	}
 	else
-		front_led_value--;
-	
-	if (front_led_value > 255) {
-		front_led_value = 254;
-		increasing = false;
-	}
-	else if (front_led_value <0) {
-		front_led_value = 1;
-		increasing = true;
-	}
+		digitalWrite(REAR_LED_PIN, LOW);
 }
